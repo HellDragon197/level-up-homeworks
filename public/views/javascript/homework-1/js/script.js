50d8b793eb9aa5f22ffc401b32576b7a8a51ec3c
helldragon197 <helldragon197@gmail.com>
2019-06-14 00:55:52 +0300

    JS HW1

---
+++ public/views/javascript/homework-1/js/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var name='Василий';
+var admin;
+admin=name;
+console.log(admin);
+
+var ourPlanetName;
+var visitorName;
+
+var framePerSec;
+var connectorBetweenWord;
+var documentView;
+var showPaginationsLastPage;
