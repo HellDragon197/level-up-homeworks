d798544e96cdadf0370d85ec037a10e51333a5a0
helldragon197 <helldragon197@gmail.com>
2019-06-24 23:14:44 +0300

    JS HW1

--- public/views/javascript/homework-1/js/script.js
+++ public/views/javascript/homework-1/js/script.js
@@ -1,17 +1,21 @@
 'use strict'
 
+// Unit link - https://learn.javascript.ru/variables#tasks
+
 // Task 1
 var name = 'Vasiliy';
 var admin;
 admin = name;
 console.log(admin);
 
-// Task 2
-var ourPlanetName;
-var visitorName;
+// Unit link - https://learn.javascript.ru/variable-names#tasks
+
+// Task 1
+var ourPlanetName = "Earth";
+var visitorName = "Petya";
 
 // Additional task 1
-var framePerSec;
-var connectorBetweenWord;
-var documentView;
-var showPaginationsLastPage;
+var fps;
+var glue;
+var viewMode;
+var showPaginationLastPage;
